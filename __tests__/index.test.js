d223614ff0c03fb3a088c202054d9a1026d89cd4
Igor Ivanov <iia02@yandex.ru>
2025-10-09 17:32:33 +0300

    Add long reverse text with fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,22 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import reverse from '../src/index.js';
 
-test('reverse', () =>{
+// Константы для работы с путями (потому что __dirname не работает в ESM)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('reverse', () => {
   expect(reverse('hello')).toEqual('olleh');
-  expect(reverse('')).toEqual('');  
+  expect(reverse('')).toEqual('');
+});
+
+test('reverse long text from file', () => {
+  const text = fs.readFileSync(getFixturePath('text.txt'), 'utf-8');
+  const expected = fs.readFileSync(getFixturePath('reversed.txt'), 'utf-8');
+
+  expect(reverse(text)).toEqual(expected);
 });
